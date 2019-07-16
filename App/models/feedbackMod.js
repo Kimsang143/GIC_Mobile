e94c177d1559eeefe9f78db0f0d35c8e53dc2026
PHAN Chanraksmey <chanraksmey.phan@gmail.com>
2019-07-16 14:51:57 +0700

    Clean model code

--- App/models/feedbackMod.js
+++ App/models/feedbackMod.js
@@ -2,15 +2,9 @@ const mongoose = require('mongoose');
 
 const feedbackSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: {
-      type: String, required: true
-    },
-    email: {
-        type: String, required: true
-     },
-    descrip: {
-      type: String, required: true
-    }
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    descrip: { type: String, required: true }
 	},
     {
   	timestamps: true
