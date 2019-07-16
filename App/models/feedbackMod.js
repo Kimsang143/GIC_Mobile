03036f36560dd7a8694118dcb1d9f3d77fcf0a1e
PHAN Chanraksmey <chanraksmey.phan@gmail.com>
2019-07-16 14:11:25 +0700

    Fix remove validator

--- App/models/feedbackMod.js
+++ App/models/feedbackMod.js
@@ -1,31 +1,15 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 const feedbackSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: {
-      type: String,
-      validate: {
-        validator: !validator.isEmpty,
-        message: 'Please fill a valid name',
-        isAsync: false
-      }
+      type: String, required: true
     },
     email: {
-        type: String,
-        validate: {
-          validator: validator.isEmail,
-          message: '{VALUE} is not a valid email',
-          isAsync: false
-        }
+        type: String, required: true
      },
     descrip: {
-      type: String,
-      validate: {
-        validator: !validator.isEmpty,
-        message: 'Please fill a valid description',
-        isAsync: false
-      }
+      type: String, required: true
     }
 	},
     {
