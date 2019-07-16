58c636cb4d8af9a72ca8c720c6a95da89a5c5209
PHAN Chanraksmey <chanraksmey.phan@gmail.com>
2019-07-16 13:09:28 +0700

    Add validator

--- App/models/Feedback.js
+++ App/models/Feedback.js
@@ -1,21 +1,16 @@
 const mongoose = require('mongoose');
-
-var validateEmail = function(email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
-};
+const validator = require('validator');
 
 const feedbackSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true },
     email: {
         type: String,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        required: 'Email address is required',
-        validate: [validateEmail, 'Please fill a valid email address'],
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        validate: {
+          validator: validator.isEmail,
+          message: '{VALUE} is not a valid email',
+          isAsync: false
+        }
      },
     descrip: { type: String, required: true }
 	},
